4326d5d36b20c62e667c119f6ee9721a177da70e
Michał Wyczarski <mwyczarski@gmail.com>
2023-11-24 19:00:46 +0100

    add lab2

---
+++ lab2/baza.pl
@@ -0,0 +1,7 @@
+zatrudnienie(anna, nowak, zelmer, duza_firma, 2000).
+zatrudnienie(agata, kowalska, siemens, duza_firma, 7000).
+zatrudnienie(tomasz, nowicki, zelmer, duza_firma, 4500).
+zatrudnienie(mateusz, chada, sbr, mala_firma, 3000).
+zatrudnienie(andrzej, uchman, frm, mala_firma, 2500).
+
+zadowolony
---
+++ lab2/rodzina.pl
@@ -0,0 +1,42 @@
+rodzic(zofia, marcin).
+rodzic(andrzej, marcin).
+rodzic(andrzej, kasia).
+rodzic(marcin, ania).
+rodzic(marcin, krzys).
+rodzic(krzys, mikolaj).
+kobieta(zofia).
+kobieta(kasia).
+kobieta(ania).
+mezczyzna(andrzej).
+mezczyzna(marcin).
+mezczyzna(krzys).
+mezczyzna(mikolaj).
+potomek(Y,X):-
+	rodzic(X,Y).
+matka(X,Y):-
+	rodzic(X,Y),
+	kobieta(X).
+dziadkowie(X,Z):-
+	rodzic(X,Y),
+	rodzic(Y,Z).
+siostra(X,Y):-
+	rodzic(Z,X),
+	rodzic(Z,Y),
+	kobieta(X).
+szczesliwy(X) :-
+	rodzic(X, Y),
+	potomek(Y, X).
+dwoje_dzieci(X) :-
+	rodzic(X, Y),
+	siostra(Y, Z), 
+	kobieta(Z).
+wnuk(Z,X) :-
+	rodzic(X,Y),
+	rodzic(Y,Z).
+ciotka(X,Y) :-
+	rodzic(Z,Y),
+	siostra(X,Z),
+	kobieta(X).
+nastepca(X,Y) :-
+	potomek(X,Z),
+	nastepca(Z,Y).
